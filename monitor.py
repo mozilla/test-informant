e8f7c295a38d8e37ac780bad020e2bd0d67da48e
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-05-30 14:08:07 -0700

    Stats per platform, separated parsing and summary, switched to TestManifest

--- monitor.py
+++ monitor.py
@@ -1,27 +1,42 @@
 import os
 import sys
 
-from manifestparser import ManifestParser
+from manifestparser import TestManifest
 
-if __name__ == '__main__':
-    src_dir_path = sys.argv[1]
+PLATFORMS = {'windows', 'linux', 'android', 'b2g'}
 
+def parse_manifests(src_dir_path):
     ini_files = set()
 
     for root, dirs, files in os.walk(src_dir_path):
+        if root[len(src_dir_path)+1:].startswith('obj'):
+            continue
         ini_files |= {os.path.join(root, f) for f in files if f.endswith('.ini')}
 
     manifests = dict()
     for ini_file in ini_files:
         try:
-            mp = ManifestParser([ini_file])
-            manifests[ini_file] = mp.tests
-        except Exception:
-            print 'Skipping ', ini_file
-            pass
+            test_manifest = TestManifest([ini_file])
+            if test_manifest.tests:
+                manifests[ini_file] = dict(total_tests=len(test_manifest.tests), platforms=dict())
+                for platform in PLATFORMS:
+                    active_tests = test_manifest.active_tests(platform)
+                    manifests[ini_file]['platforms'][platform] = dict(active_tests=active_tests)
+        except Exception as e:
+            print 'Skipping {}. Exception: {}.'.format(ini_file, e)
 
-    print '{} manifests found.'.format(len(manifests))
+    return manifests
+
+def summarize_manifests(manifests):
+    print '# {} manifests found.'.format(len(manifests))
     print
 
-    for manifest, tests in manifests.iteritems():
-        print '  - Manifest "{}": {} tests.'.format(manifest, len(tests))
+    for manifest, manifest_data in manifests.iteritems():
+        print '. Manifest "{}": {} total tests.'.format(manifest, manifest_data['total_tests'])
+        for platform, platform_data in manifest_data['platforms'].iteritems():
+            print '    - Platform "{}": {} tests.'.format(platform, len(platform_data['active_tests']))
+
+if __name__ == '__main__':
+    src_dir_path = sys.argv[1]
+    manifests = parse_manifests(src_dir_path)
+    summarize_manifests(manifests)
