2ac96c006aebb1569920a3cd85391e3f75017c49
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-06-03 11:53:03 -0700

    Changed to parsing manifests with different options (os, bits, ...)
    Also using argparse for the cli

--- monitor.py
+++ monitor.py
@@ -1,27 +1,34 @@
+"""Monitors tests manifest files"""
+
+import argparse
+import json
 import os
 import sys
 
 from manifestparser import TestManifest
 
-PLATFORMS = {'windows', 'linux', 'android', 'b2g'}
+choices = {'os': ['linux', 'bsd', 'win', 'mac', 'unix'],
+           'bits': [32, 64],
+           'processor': ['x86', 'x86_64', 'ppc']}
 
-def parse_manifests(src_dir_path):
+def get_ini_files(src_dir_path):
     ini_files = set()
-
     for root, dirs, files in os.walk(src_dir_path):
         if root[len(src_dir_path)+1:].startswith('obj'):
             continue
         ini_files |= {os.path.join(root, f) for f in files if f.endswith('.ini')}
+    return ini_files
 
+def parse_manifests(ini_files, options):
     manifests = dict()
     for ini_file in ini_files:
         try:
             test_manifest = TestManifest([ini_file])
             if test_manifest.tests:
-                manifests[ini_file] = dict(total_tests=len(test_manifest.tests), platforms=dict())
-                for platform in PLATFORMS:
-                    active_tests = test_manifest.active_tests(platform)
-                    manifests[ini_file]['platforms'][platform] = dict(active_tests=active_tests)
+                active_tests = test_manifest.active_tests(**options)
+                manifests[ini_file] = dict(total_tests=len(test_manifest.tests),
+                                           active_tests=len(active_tests),
+                                           tests=active_tests)
         except Exception as e:
             print 'Skipping {}. Exception: {}.'.format(ini_file, e)
 
@@ -32,11 +39,28 @@ def summarize_manifests(manifests):
     print
 
     for manifest, manifest_data in manifests.iteritems():
-        print '. Manifest "{}": {} total tests.'.format(manifest, manifest_data['total_tests'])
-        for platform, platform_data in manifest_data['platforms'].iteritems():
-            print '    - Platform "{}": {} tests.'.format(platform, len(platform_data['active_tests']))
+        print '. Manifest "{}": {} total tests. {} active tests.'.format(manifest, manifest_data['total_tests'], manifest_data['active_tests'])
+    print '-' * 80
+    print '{} ...'.format(json.dumps(manifests, indent=2)[:800])
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('path', type=str,
+                         help="path to the source directory")
+    for key in choices:
+        parser.add_argument('--%s' % key, dest=key,
+                           nargs='?',
+                           help="display choices for %s" % key)
+    args = parser.parse_args()
+    path = args.path
+    del args.path
+    options = dict(args.__dict__)
+
+    print "Options: {}".format(options)
+
+    ini_files = get_ini_files(path)
+    manifests = parse_manifests(ini_files, options)
+    summarize_manifests(manifests)
 
 if __name__ == '__main__':
-    src_dir_path = sys.argv[1]
-    manifests = parse_manifests(src_dir_path)
-    summarize_manifests(manifests)
+    main()
