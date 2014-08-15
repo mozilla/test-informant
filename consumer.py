18638cb9cd624fe90097ad00acb46152e3b908e1
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-08-15 12:10:48 -0700

    Updated consumer, now creates revision objects. Update logging.

--- consumer.py
+++ consumer.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python
-from mozillapulse import consumers
+import datetime
+import logging
+import sys
+
+import mongoengine
+import mozillapulse.consumers
+
+import config
+from models import CodeRevision
+
+# Setting up logging
+log = logging.getLogger('manifestmonitor')
+log.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+log.addHandler(handler)
 
 def message_handler(data, message):
     # Do something with the message data
-    print "The script got a message from pulse"
     payload = data['payload']
-
-    revision = payload['revision']
+    revision_id = payload['revision']
     tests_url = payload['testsurl']
-    print "Revision: {} ; tests.zip url is: {}".format(revision, tests_url)
+    date = datetime.datetime.fromtimestamp(payload['builddate'])
+
+    try:
+        rev = CodeRevision.objects.get(id=revision_id)
+        log.info("Revision {} already exists".format(revision_id))
+    except mongoengine.DoesNotExist:
+        rev = CodeRevision(id=revision_id, tests_url=tests_url, date=date)
+        rev.save()
+        log.info("Created a new revision: {}".format(revision_id))
 
     # Ack the message to tell pulse we processed it
     message.ack()
@@ -16,9 +36,10 @@ def message_handler(data, message):
 def main():
     import uuid
     unique_label = 'manifestmonitor-%s' % uuid.uuid4()
+    mongoengine.connect(config.db_name)
 
     # Connect to pulse
-    pulse = consumers.NormalizedBuildConsumer(applabel=unique_label)
+    pulse = mozillapulse.consumers.NormalizedBuildConsumer(applabel=unique_label)
 
     # Tell pulse that you want to listen for all messages ('#' is everything)
     # and give a function to call every time there is a message
