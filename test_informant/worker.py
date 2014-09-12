1be4ff84d520a579b33a04a11460012f18ab0435
Andrew Halberstadt <halbersa@gmail.com>
2014-09-12 12:23:18 -0400

    Add a way to bypass the tests_cache, needed for certain platform/suites like android xpcshell

--- test_informant/worker.py
+++ test_informant/worker.py
@@ -49,54 +49,66 @@ class Worker(threading.Thread):
         build_str = "{}-{}-{}".format(data['buildid'], data['platform'], data['buildtype'])
         self.log("now processing build '{}'".format(build_str))
 
-        tests_path = self._prepare_tests(data['revision'], data['testsurl'])
+        # some platforms (i.e android, b2g) have a different set of xpcshell manifests
+        # copied to the tests.zip.
+        use_cache = True
+        if data['platform'] in ('android',):
+            use_cache = False
 
-        # compute mozinfo.json url based off the tests.zip url
-        mozinfo_url = '{}.mozinfo.json'.format(data['testsurl'][:-len('.tests.zip')])
-        mozinfo_path = self._prepare_mozinfo(mozinfo_url)
-        with open(mozinfo_path, 'r') as f:
-            mozinfo_json = json.loads(f.read())
-        mozfile.remove(mozinfo_path)
+        tests_path = self._prepare_tests(data['revision'], data['testsurl'], use_cache=use_cache)
+        try:
+            # compute mozinfo.json url based off the tests.zip url
+            mozinfo_url = '{}.mozinfo.json'.format(data['testsurl'][:-len('.tests.zip')])
+            mozinfo_path = self._prepare_mozinfo(mozinfo_url)
+            with open(mozinfo_path, 'r') as f:
+                mozinfo_json = json.loads(f.read())
+            mozfile.remove(mozinfo_path)
 
-        build = Build(
-            buildid=data['buildid'],
-            buildtype=data['buildtype'],
-            platform=data['platform'],
-            config=mozinfo_json,
-            timestamp=data['builddate'],
-            revision=data['revision'],
-        )
-
-        for suite in config.PLATFORMS[(data['platform'], data['buildtype'])]:
-            manifests = [os.path.join(tests_path, m) for m in config.SUITES[suite]['manifests']]
-            parse = config.SUITES[suite]['parser']()
-            active, skipped = parse(manifests, mozinfo_json)
-
-            # only store relative paths
-            active = [os.path.relpath(t, tests_path) for t in active]
-            skipped = [os.path.relpath(t, tests_path) for t in skipped]
-
-            # keep track of totals for the entire build across all test suites
-            build.total_active_tests += len(active)
-            build.total_skipped_tests += len(skipped)
-
-            # don't store multiple copies of the same result
-            s, created = Suite.objects.get_or_create(
-                name=suite,
-                active_tests=active,
-                skipped_tests=skipped,
+            # create an entry for this build in the db
+            build = Build(
+                buildid=data['buildid'],
+                buildtype=data['buildtype'],
+                platform=data['platform'],
+                config=mozinfo_json,
+                timestamp=data['builddate'],
+                revision=data['revision'],
             )
-            s.refcount += 1
-            s.save()
-            build.suites.append(s)
-        build.save()
+
+            for suite in config.PLATFORMS[(data['platform'], data['buildtype'])]:
+                manifests = [os.path.join(tests_path, m) for m in config.SUITES[suite]['manifests']]
+                parse = config.SUITES[suite]['parser']()
+
+                # perform the actual manifest parsing
+                active, skipped = parse(manifests, mozinfo_json)
+
+                # only store relative paths
+                active = [os.path.relpath(t, tests_path) for t in active]
+                skipped = [os.path.relpath(t, tests_path) for t in skipped]
+
+                # keep track of totals for the entire build across all test suites
+                build.total_active_tests += len(active)
+                build.total_skipped_tests += len(skipped)
+
+                # don't store multiple copies of the same result
+                s, created = Suite.objects.get_or_create(
+                    name=suite,
+                    active_tests=active,
+                    skipped_tests=skipped,
+                )
+                s.refcount += 1
+                s.save()
+                build.suites.append(s)
+            build.save()
+        finally:
+            if not use_cache:
+                mozfile.remove(tests_path)
         self.log("finished processing build '{}'.".format(build_str))
 
     def log(self, message):
         print("{} - {}".format(self.name, message))
 
-    def _prepare_tests(self, revision, tests_url):
-        if revision in tests_cache:
+    def _prepare_tests(self, revision, tests_url, use_cache=True):
+        if use_cache and revision in tests_cache:
             # the tests bundle is possibly being downloaded by another thread,
             # wait a bit before downloading ourselves.
             timeout = 300 # 5 minutes
@@ -109,11 +121,14 @@ class Worker(threading.Thread):
                 time.sleep(1)
 
         self.log("downloading tests bundle for revision '{}'".format(revision))
-        # let other threads know we are already downloading this rev
-        tests_cache[revision] = None
-        if len(tests_cache) >= config.MAX_TESTS_CACHE_SIZE:
-            # clean up the oldest revision, it most likely isn't needed anymore
-            mozfile.remove(tests_cache.popitem(last=False)[1]) # FIFO
+
+        if use_cache:
+            # let other threads know we are already downloading this rev
+            tests_cache[revision] = None
+
+            if len(tests_cache) >= config.MAX_TESTS_CACHE_SIZE:
+                # clean up the oldest revision, it most likely isn't needed anymore
+                mozfile.remove(tests_cache.popitem(last=False)[1]) # FIFO
 
         tf = mozfile.NamedTemporaryFile()
         with open(tf.name, 'wb') as f:
@@ -122,7 +137,8 @@ class Worker(threading.Thread):
         tests_path = tempfile.mkdtemp()
         mozfile.extract(tf.name, tests_path)
 
-        tests_cache[revision] = tests_path
+        if use_cache:
+            tests_cache[revision] = tests_path
         return tests_path
 
     def _prepare_mozinfo(self, mozinfo_url):
