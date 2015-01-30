fe74115b768cceb026dd54eb54a798481be7f354
Alice Scarpa <alicescarpa@gmail.com>
2015-01-30 15:14:27 -0500

    Bug 1125863 - differentiating enabled from added and disabled from removed

--- tools/formatters/base.py
+++ tools/formatters/base.py
@@ -44,28 +44,38 @@ class BaseFormatter(object):
             total_suite_active = 0
             total_suite_added = 0
             total_suite_removed = 0
+            total_suite_enabled = 0
+            total_suite_disabled = 0
 
             for platform, tests in platforms.iteritems():
-                added = removed = []
+                added = removed = enabled = disabled = []
                 if suite in from_suites and platform in from_suites[suite]:
-                    added = [t for t in tests['active'] if t not in from_suites[suite][platform]['active']]
-                    removed = [t for t in from_suites[suite][platform]['active'] if t not in tests['active']]
+                    added = [t for t in tests['active'] if t not in from_suites[suite][platform]['active'] and t not in from_suites[suite][platform]['skipped']]
+                    removed = [t for t in from_suites[suite][platform]['active'] if t not in tests['active'] and t not in tests['skipped']]
+                    enabled = [t for t in tests['active'] if t not in from_suites[suite][platform]['active'] and t in from_suites[suite][platform]['skipped']]
+                    disabled = [t for t in from_suites[suite][platform]['active'] if t not in tests['active'] and t in tests['skipped']]
 
                 data['suites'][suite][platform]['total'] =  len(tests['active']) + len(tests['skipped'])
                 data['suites'][suite][platform]['active'] = len(tests['active'])
                 data['suites'][suite][platform]['skipped'] = tests['skipped']
                 data['suites'][suite][platform]['added'] = added
                 data['suites'][suite][platform]['removed'] = removed
+                data['suites'][suite][platform]['enabled'] = enabled
+                data['suites'][suite][platform]['disabled'] = disabled
 
                 total_suite_tests += len(tests['active']) + len(tests['skipped'])
                 total_suite_active += len(tests['active'])
                 total_suite_added += len(added)
                 total_suite_removed += len(removed)
+                total_suite_enabled += len(enabled)
+                total_suite_disabled += len(disabled)
 
             data['suites'][suite]['meta']['total'] = total_suite_tests
             data['suites'][suite]['meta']['active'] = total_suite_active
             data['suites'][suite]['meta']['added'] = total_suite_added
             data['suites'][suite]['meta']['removed'] = total_suite_removed
+            data['suites'][suite]['meta']['enabled'] = total_suite_enabled
+            data['suites'][suite]['meta']['disabled'] = total_suite_disabled
             data['total_tests'] += total_suite_tests
             data['total_active'] += total_suite_active
         return data
