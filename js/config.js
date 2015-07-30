23af139d5959a3d845c17d663c985258e2c46aba
Andrew Halberstadt <halbersa@gmail.com>
2015-07-30 14:13:03 -0400

    Populate form suites and branches from config file

---
+++ js/config.js
@@ -0,0 +1,118 @@
+function dxr(testName) {
+
+}
+
+var SUITES = {
+  'androidx86-set': {
+    displayName: 'androidx86-set',
+    urlFormatter: dxr
+  },
+  'gaia-ui-test-a11y': {
+    displayName: 'gaia-ui-test-a11y',
+    urlFormatter: dxr
+  },
+  'gaia-ui-test-functional': {
+    displayName: 'gaia-ui-test-functional',
+    urlFormatter: dxr
+  },
+  'gaia-ui-test-unit': {
+    displayName: 'gaia-ui-test-unit',
+    urlFormatter: dxr
+  },
+  'gaia-unit': {
+    displayName: 'gaia-unit',
+    urlFormatter: dxr
+  },
+  'marionette': {
+    displayName: 'marionette',
+    urlFormatter: dxr
+  },
+  'marionette-e10s': {
+    displayName: 'marionette-e10s',
+    urlFormatter: dxr
+  },
+  'marionette-webapi': {
+    displayName: 'marionette-webapi',
+    urlFormatter: dxr
+  },
+  'mochitest-a11y': {
+    displayName: 'mochitest-a11y',
+    urlFormatter: dxr
+  },
+  'mochitest-browser-chrome': {
+    displayName: 'mochitest-browser-chrome',
+    urlFormatter: dxr
+  },
+  'mochitest-e10s-browser-chrome': {
+    displayName: 'mochitest-browser-chrome-e10s',
+    urlFormatter: dxr
+  },
+  'mochitest-chrome': {
+    displayName: 'mochitest-chrome',
+    urlFormatter: dxr
+  },
+  'mochitest-debug': {
+    displayName: 'mochitest-debug',
+    urlFormatter: dxr
+  },
+  'mochitest-devtools-chrome': {
+    displayName: 'mochitest-devtools-chrome',
+    urlFormatter: dxr
+  },
+  'mochitest-e10s-devtools-chrome': {
+    displayName: 'mochitest-devtools-chrome-e10s',
+    urlFormatter: dxr
+  },
+  'mochitest-gl': {
+    displayName: 'mochitest-gl',
+    urlFormatter: dxr
+  },
+  'mochitest-jetpack': {
+    displayName: 'mochitest-jetpack',
+    urlFormatter: dxr
+  },
+  'mochitest-oop': {
+    displayName: 'mochitest-oop',
+    urlFormatter: dxr
+  },
+  'mochitest-other': {
+    displayName: 'mochitest-other',
+    urlFormatter: dxr
+  },
+  'mochitest': {
+    displayName: 'mochitest-plain',
+    urlFormatter: dxr
+  },
+  'mochitest-e10s': {
+    displayName: 'mochitest-plain-e10s',
+    urlFormatter: dxr
+  },
+  'robocop': {
+    displayName: 'robocop',
+    urlFormatter: dxr
+  },
+  'xpcshell': {
+    displayName: 'xpcshell',
+    urlFormatter: dxr
+  },
+  'web-platform-tests': {
+    displayName: 'web-platform-tests',
+    urlFormatter: dxr
+  },
+  'web-platform-tests-reftests': {
+    displayName: 'web-platform-tests-reftests',
+    urlFormatter: dxr
+  }
+};
+
+var BRANCHES = [
+  'mozilla-central',
+  'mozilla-inbound',
+  'b2g-inbound',
+  'fx-team',
+  'mozilla-aurora',
+  'mozilla-beta',
+  'mozilla-release'
+];
+
+  
